feba11fdcf029aa2216b134b1b74f1cad900658b
AlexGrohmann <alex.grohmann@googlemail.com>
2024-03-17 19:39:19 +0100

    add shortcuts json

--- shortcuts.py
+++ shortcuts.py
@@ -1,3 +1,16 @@
-print("Hello I return shortcuts for Windows and Mac (incl. Visual Studio Code)")
-print("Input: copy")
-print("Output: Windows: strg + c, Mac: ??? + c")
+# print("Hello I return shortcuts for Windows and Mac (incl. Visual Studio Code)")
+# print("Input: copy")
+# print("Output: Windows: strg + c, Mac: ??? + c")
+import json
+
+# Retrieve JSON data from the file
+with open("shortcuts.json", "r") as file:
+    data = json.load(file)
+
+input = input("insert shortcut\n")
+# Access and process the retrieved JSON data
+for shortcut in data["shortcuts"]:
+    for key in shortcut["type"]:
+        if key == input:
+            print("Windows: " + shortcut["windows"])
+            print("Mac: " + shortcut["mac"])
